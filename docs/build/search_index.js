bff49b8c1d64b75b8b8686b76fc8a287ad5f238a
Christoph Lenovo <christophhedtrich@gmail.com>
2019-05-16 13:25:46 +0200

    add docs built

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DEMC.jl-1","page":"Home","title":"DEMC.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [DEMC]","category":"page"}]
+}
