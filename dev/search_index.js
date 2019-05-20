d6e23d5026754a4582eb3252999d4a4591fb0b34
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-05-20 13:22:35 +0000

    build based on 7c2d3b1

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#DEMC.jl-1","page":"Home","title":"DEMC.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [DEMC]","category":"page"}]
+[{"location":"#DEMC.jl-1","page":"Home","title":"DEMC.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [DEMC]","category":"page"},{"location":"#DEMC.demcz_sample_par","page":"Home","title":"DEMC.demcz_sample_par","text":"test this\n\n\n\n\n\n","category":"function"}]
 }
