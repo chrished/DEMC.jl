cbb10c5225e399c15bc539f486cb143942b3a443
Christoph Lenovo <christophhedtrich@gmail.com>
2019-05-28 13:52:24 +0200

    add annealing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,17 @@
 using DEMC
 using Test
 
-@testset "DEMC.jl" begin
+@testset "DEMCz Sampling" begin
     # Write your own tests here.
     println("test with norm pdf in serial computation")
     include("example_normpdf.jl")
     println("test with norm pdf in parallel computation")
     include("example_normpdf_parallel.jl")
+end
+
+@testset "DEMCz Annealing" begin
+    println("test annealing with quadratic optimization")
+    include("test_anneal.jl")
+    println("test annealing with quadratic optimization in parallel")
+    include("test_anneal_parallel.jl")
 end
